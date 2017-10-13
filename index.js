21bd66e42b4fdedb8f651ae946d1742c02305909
Sammy W <swucode@gmail.com>
2017-10-13 05:31:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/*
-Variables defined in the test suite:
-- oldID
-- currentAge
+const newID = oldID + 1000000000;
 
-You should NOT try to redefine any of the above variables.
-*/
+const ageIsValid = Number.isInteger(currentAge);
 
-// Write your code below this line!
+const randomNumber = Math.random() * 10 + 10;
+
+const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = (randomInteger + 1000000001);
